bf731d78936b145be52877626e5ecbd8c3231a6b
Dodman101 <dodmanmwaura@gmail.com>
2024-07-08 20:01:17 +0300

    Started day 17 completed the extraction of the questions part.

---
+++ Day 17/main.py
@@ -0,0 +1,12 @@
+from question_model import Question
+from data import question_data
+import random
+
+question_bank = []
+
+for question in question_data:
+    question_text = question["text"]
+    question_answer = question["answer"]
+    new_question = Question(q_text=question_text, q_answer=question_answer)
+    question_bank.append(new_question)
+
